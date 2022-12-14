e162ba7e8df3127ca2043188ccd7df22b76daf61
alarvt <alarvt@gmail.com>
2022-12-14 23:00:57 +0100

    ДЗ-9.1

--- src/src/js/01-color-switcher.js
+++ src/src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
 
+const startBtn = document.querySelector('button[data-start="start"');
+const stopBtn = document.querySelector('button[data-stop="stop"');
+      
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+    
+const bodyColor = document.body;
+
+const colorChenger = {
+  isActive: false,
+    startColor() {
+        if (this.isActive) {
+            return;
+        }
+        this.isActive = true;
+        this.    = setInterval(() => {
+          bodyColor.style.backgroundColor = getRandomHexColor();
+          console.log(bodyColor.style.backgroundColor);
+        }, 1000);
+    },
+    stopColor() {
+        this.isActive = fasle;
+        clearInterval(this.intervalId);
+    }
+};
+
+startBtn.addEventListener('click', e => {
+    colorChenger.startColor()
+});
+
+stopBtn.addEventListener('click', e => {
+  colorChenger.stopColor()
+});
+
+
+
