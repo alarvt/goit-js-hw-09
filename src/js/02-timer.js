9d52bad9443b6a7bfe899b5dd987a6ce47728f24
alarvt <alarvt@gmail.com>
2022-12-20 16:33:54 +0100

    ДЗ-9.10

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,62 +1,81 @@
 import flatpickr from 'flatpickr';
-// Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
+require('flatpickr/dist/themes/dark.css');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-import Notiflix from 'notiflix';
+const refs = {
+  btnTimerStart: document.querySelector('[data-start]'),
+  timerFieldDays: document.querySelector('[data-days]'),
+  timerFielHours: document.querySelector('[data-hours]'),
+  timerFieldMinutes: document.querySelector('[data-minutes]'),
+  timerFieldSeconds: document.querySelector('[data-seconds]'),
+};
 
-const input = document.querySelector("#datetime - picker");
-const Btn = document.querySelector('button[data-start="start"');
+refs.btnTimerStart.disabled = true;
+let timerId = null;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const currentDate = new Date();
+
+    if (selectedDates[0] - currentDate > 0) {
+      refs.btnTimerStart.disabled = false;
+    } else {
+      refs.btnTimerStart.disabled = true;
+      Notify.failure('Please choose a date in the future', {
+        timeout: 1500,
+        width: '400px',
+      });
+    }
   },
 };
 
-/* function onInputDate(selectedDates) {
-  if (selectedDates <= Date.now()) {   
-    Notiflix.Notify.failure('Please choose a date in the future');
-  } else {
-    refs.buttonStartEl.removeAttribute('disabled', 'disabled');
-    onStartedTimer(selectedDates);
-  }
-} */
-
 function convertMs(ms) {
-
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
- 
   const days = Math.floor(ms / day);
-  
   const hours = Math.floor((ms % day) / hour);
- 
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+}
 
-/* {
-    altInput: true,
-    altFormat: "F j, Y",
-    dateFormat: "Y-m-d",
-} */
+function onTimerStart() {
+  const selectedDate = fp.selectedDates[0];
 
-//-------------------------
+  timerId = setInterval(() => {
+    const startTime = new Date();
+    const countdown = selectedDate - startTime;
+    refs.btnTimerStart.disabled = true;
 
+    if (countdown < 0) {
+      clearInterval(timerId);
+      return;
+    }
+    updateTimerFace(convertMs(countdown));
+  }, 1_000);
+}
 
-const date = new Date();
-console.log(date)
+function updateTimerFace({ days, hours, minutes, seconds }) {
+  refs.timerFieldDays.textContent = addLeadingZero(days);
+  refs.timerFielHours.textContent = addLeadingZero(hours);
+  refs.timerFieldMinutes.textContent = addLeadingZero(minutes);
+  refs.timerFieldSeconds.textContent = addLeadingZero(seconds);
+}
+
+const fp = flatpickr('#datetime-picker', options);
+
+refs.btnTimerStart.addEventListener('click', onTimerStart);
